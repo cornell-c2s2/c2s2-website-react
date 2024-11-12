4f70fe0ada351998127396b237d9f0054edfb647
Nicholas Channg <87846996+NicholasChanng@users.noreply.github.com>
2024-11-12 17:19:31 -0500

    added sponsors page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Layout from "./components/layout.tsx";

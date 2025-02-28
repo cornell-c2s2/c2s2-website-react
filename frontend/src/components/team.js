f71623b4bbe106bdfd3c4d8544b2a99c68f35003
Nicholas Channg <87846996+NicholasChanng@users.noreply.github.com>
2025-02-28 12:55:08 -0500

    finished alumni section

--- frontend/src/components/team.js
+++ frontend/src/components/team.js
@@ -563,15 +563,18 @@ function Team() {
         </div>
       </section>
 
-      <section>
+      <section className="alumni-section">
         <div className="container">
           <div className="section-title" data-aos="fade-up">
-            {/* <i className="bi bi-clipboard-check"></i> */}
             <p>Alumni</p>
           </div>
-          <div className="row justify-content-center">
-            {AlumniInfo.map((alumni) => {
-              return Alumni(alumni);
+          <div className="inner-page" data-aos="fade-up">
+            <p>Thanks to our amazing Alumni who continue to support us!</p>
+          </div>
+
+          <div className="alumni-grid">
+            {AlumniInfo.map((alumni, index) => {
+              return <Alumni key={index} {...alumni} />;
             })}
           </div>
         </div>
@@ -629,12 +632,43 @@ function TeamMember(prop) {
 }
 
 function Alumni(prop) {
+  // Get initials for the avatar
+  const getInitials = (firstName, lastName) => {
+    return (firstName?.[0] || "") + (lastName?.[0] || "");
+  };
+
+  const initials = getInitials(prop.first, prop.last);
+
   return (
-    <div className="alumni">
-      <div>{prop.first + " " + prop.last}</div>
-      <a href={prop.linkedin} target="_blank">
-        LinkedIn
-      </a>
+    <div className="alumni-card">
+      <div className="avatar">
+        <span>{initials}</span>
+      </div>
+
+      <div className="name">{prop.first + " " + prop.last}</div>
+
+      <div className="linkedin-container">
+        {prop.linkedin === "" ? (
+          <span className="no-linkedin">No LinkedIn profile</span>
+        ) : (
+          <a
+            href={prop.linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="linkedin-link"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="linkedin-icon"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+            </svg>
+            <span>LinkedIn</span>
+          </a>
+        )}
+      </div>
     </div>
   );
 }
